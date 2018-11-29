5da2df292a242225813252e670daf408cd568074
chuckfairy <charlieabeling@gmail.com>
2018-11-29 13:21:54 -0600

    add gitconfig vim install add

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@
 dir=$( dirname $0 )
 ln -s $dir/.gdbinit ~/.gdbinit
 ln -s $dir/.bash_profile ~/.bash_profile
+
+ln -s $dir/.gitconfig ~/.gitconfig
+
+# Vim Install
+git clone https://github.com/chuckfairy/vim-files ~/.vim
+~/.vim/install.sh
