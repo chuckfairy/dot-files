fec0cfcf05b1719a8963a8fdc9e7122d265ee305
chuckfairy <charlieabeling@gmail.com>
2020-06-13 14:07:16 -0500

    tmux install add

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ cp $dir/.gitconfig $HOME/.gitconfig
 # Vim Install
 git clone https://github.com/chuckfairy/vim-files $HOME/.vim
 $HOME/.vim/install.sh
+
+# TMUX
+git clone https://github.com/chuckfairy/tmux-files $HOME/.tmux
+cp $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
